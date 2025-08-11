5cbf80dc86033bd2280c7722b6beea41057c3e46
abraham hailegebriel <abrahamhailegebriel@email.com>
2025-08-11 16:19:11 +0300

    Update Navbar component and modify layout for login button

---
+++ components/nav/Loginform.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../ui/button'
+// import Button from '@mui/material/Button';
+export default function Loginform() {
+  return (
+    <Button variant="outline">
+         
+        Login
+        </Button>
+  )
+}
+
